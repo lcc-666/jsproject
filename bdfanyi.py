b34487faaa3811a0a76112b6ec24fc6021449bc2
lcc-666 <2502247952@qq.com>
2021-12-23 22:39:03 +0800

    百度翻译两种方法(sug/sign)

--- bdfanyi.py
+++ bdfanyi.py
@@ -15,6 +15,7 @@ sign: 871501.634748
 token: 223c0d460a3e1417ca97feb683533ada
 domain: common"""
 
+import execjs
 import requests
 import re
 def str2dict(formstr):
@@ -23,7 +24,7 @@ def str2dict(formstr):
     for i in r:
         form[i[0].strip()] = i[1].strip()
     return form
-
+#请求头
 head="""Accept: */*
 Accept-Encoding: gzip, deflate, br
 Accept-Language: en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7
@@ -41,21 +42,74 @@ Sec-Fetch-Mode: cors
 Sec-Fetch-Site: same-origin
 User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36
 X-Requested-With: XMLHttpRequest"""
-#sug简单爬取
+
 word=input("请输入要翻译的单词\n")
 headers=str2dict(head)
-data={
-    'kw':word
-}
-html=requests.post(url="https://fanyi.baidu.com/sug",data=data,headers=headers).json()
 
-print(html)
-
+#sug简单爬取
 # data={
-#     'from': 'en',
-#     'to': 'zh',
-#     'query': word,
-#     'simple_means_flag': '3',
-#     'sign': '871501.634748',
-#     'token': '223c0d460a3e1417ca97feb683533ada'
+#     'kw':word
 # }
+# html=requests.post(url="https://fanyi.baidu.com/sug",data=data,headers=headers).json()
+#
+# print(html)
+
+#获取sign爬取
+
+js="""     
+    function n(r, o) {
+        for (var t = 0; t < o.length - 2; t += 3) {
+            var a = o.charAt(t + 2);
+            a = a >= "a" ? a.charCodeAt(0) - 87 : Number(a),
+            a = "+" === o.charAt(t + 1) ? r >>> a : r << a,
+            r = "+" === o.charAt(t) ? r + a & 4294967295 : r ^ a
+        }
+        return r
+    }
+
+    function e(r) {
+        var i = "320305.131321201"
+        var o = r.match(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g);
+        if (null === o) {
+            var t = r.length;
+            t > 30 && (r = "" + r.substr(0, 10) + r.substr(Math.floor(t / 2) - 5, 10) + r.substr(-10, 10))
+        } else {
+            for (var e = r.split(/[\uD800-\uDBFF][\uDC00-\uDFFF]/), C = 0, h = e.length, f = []; h > C; C++)
+                "" !== e[C] && f.push.apply(f, a(e[C].split(""))),
+                C !== h - 1 && f.push(o[C]);
+            var g = f.length;
+            g > 30 && (r = f.slice(0, 10).join("") + f.slice(Math.floor(g / 2) - 5, Math.floor(g / 2) + 5).join("") + f.slice(-10).join(""))
+        }
+        var u = void 0
+          , l = "" + String.fromCharCode(103) + String.fromCharCode(116) + String.fromCharCode(107);
+        u = null !== i ? i : (i = window[l] || "") || "";
+        for (var d = u.split("."), m = Number(d[0]) || 0, s = Number(d[1]) || 0, S = [], c = 0, v = 0; v < r.length; v++) {
+            var A = r.charCodeAt(v);
+            128 > A ? S[c++] = A : (2048 > A ? S[c++] = A >> 6 | 192 : (55296 === (64512 & A) && v + 1 < r.length && 56320 === (64512 & r.charCodeAt(v + 1)) ? (A = 65536 + ((1023 & A) << 10) + (1023 & r.charCodeAt(++v)),
+            S[c++] = A >> 18 | 240,
+            S[c++] = A >> 12 & 63 | 128) : S[c++] = A >> 12 | 224,
+            S[c++] = A >> 6 & 63 | 128),
+            S[c++] = 63 & A | 128)
+        }
+        for (var p = m, F = "" + String.fromCharCode(43) + String.fromCharCode(45) + String.fromCharCode(97) + ("" + String.fromCharCode(94) + String.fromCharCode(43) + String.fromCharCode(54)), D = "" + String.fromCharCode(43) + String.fromCharCode(45) + String.fromCharCode(51) + ("" + String.fromCharCode(94) + String.fromCharCode(43) + String.fromCharCode(98)) + ("" + String.fromCharCode(43) + String.fromCharCode(45) + String.fromCharCode(102)), b = 0; b < S.length; b++)
+            p += S[b],
+            p = n(p, F);
+        return p = n(p, D),
+        p ^= s,
+        0 > p && (p = (2147483647 & p) + 2147483648),
+        p %= 1e6,
+        p.toString() + "." + (p ^ m)
+    }"""
+
+ctx=execjs.compile(js)
+data={
+    'from': 'en',
+    'to': 'zh',
+    'query': word,
+    'simple_means_flag': '3',
+    'sign': ctx.call("e",word),
+    'token': '223c0d460a3e1417ca97feb683533ada'
+}
+
+html=requests.post(url="https://fanyi.baidu.com/v2transapi?from=en&to=zh",data=data,headers=headers).json()
+print(html['trans_result']['data'][0]['dst'])
