5cde05ca54297e2e302c5c0ebc79133b7660f06c
lcc-666 <2502247952@qq.com>
2022-04-25 20:33:06 +0800

    第一次实验

--- experiment/one/req1.py
+++ experiment/one/req1.py
@@ -13,13 +13,14 @@ news = tree.xpath("/html/body/div[4]/div[1]/div[3]/ul/li/a/@title")
 print("学校新闻")
 for item in news:
     print(item)
+#
+# notice=tree.xpath("/html/body/div[6]/div/div[1]/ul/li/a/@title")
+# print("通知")
+# for item in notice:
+#     print(item)
 
-notice=tree.xpath("/html/body/div[6]/div/div[1]/ul/li/a/@title")
-print("通知")
-for item in notice:
-    print(item)
-
-url = "https://news.sxau.edu.cn/"
+url = "https://pic.netbian.com/4kmeinv"
+root = "https://pic.netbian.com"
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
                   "Chrome/100.0.4896.127 Safari/537.36 Edg/100.0.1185.50 "
@@ -28,3 +29,11 @@ req = requests.get(url, headers=headers, timeout=2)
 req.encoding = req.apparent_encoding
 tree = etree.HTML(req.text)
 
+pics = tree.xpath("//*[@id='main']/div[3]/ul/li/a/@href")
+print("\n拼接前")
+for pic in pics[:3]:
+    print(pic)
+
+print("拼接后")
+for pic in pics[:3]:
+    print(root + pic)
