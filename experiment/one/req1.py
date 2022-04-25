3012602f409d5c8bb0e4ed4c74386233c50cfbbd
lcc-666 <2502247952@qq.com>
2022-04-25 20:27:49 +0800

    配置信息

--- experiment/one/req1.py
+++ experiment/one/req1.py
@@ -19,3 +19,12 @@ print("通知")
 for item in notice:
     print(item)
 
+url = "https://news.sxau.edu.cn/"
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                  "Chrome/100.0.4896.127 Safari/537.36 Edg/100.0.1185.50 "
+}
+req = requests.get(url, headers=headers, timeout=2)
+req.encoding = req.apparent_encoding
+tree = etree.HTML(req.text)
+
