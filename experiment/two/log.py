4b943bc25752610ff835e979e4a3d2c292ce21eb
lcc-666 <2502247952@qq.com>
2022-05-02 20:43:46 +0800

    增加解释说明

--- experiment/two/log.py
+++ experiment/two/log.py
@@ -1,3 +1,9 @@
+"""
+我已经手动确定了驱动的路径，不需要将其配置到python解释环境之中
+chrome版本采用最新的101
+selenium使用selenium3，如果使用selenium4会出现兼容问题
+"""
+
 from selenium import webdriver
 import time
 
@@ -12,7 +18,7 @@ bro.find_element_by_id('switcher_plogin').click()
 username_tage = bro.find_element_by_id('u')
 password_tage = bro.find_element_by_id('p')
 
-username_tage.send_keys("你的qq")
+username_tage.send_keys("你的QQ")
 password_tage.send_keys("你的密码")
 time.sleep(2)
 
